a903cd844197291f3c3a0bdcccd9a3ab969bb559
MarvinPoulsen <marpo@lolland.dk>
2023-11-16 14:34:09 +0100

    initial commit

---
+++ internal/src/components/app/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC, useRef, useState } from 'react';
+import './app.scss';
+
+const App: FC = () => {
+
+    return (
+        <><p>Hallo World</p>
+        </>
+    );
+};
+
+export default App;
