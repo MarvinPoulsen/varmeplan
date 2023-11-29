35eeabc6efb79d6b933cddd7f27492a647799b32
MarvinPoulsen <marpo@lolland.dk>
2023-11-29 15:03:58 +0100

    update datasources

--- internal/config.ts
+++ internal/config.ts
@@ -1,3 +1,15 @@
-const varmeplanMinimapIdParam = '[module.varmeplan.test.minimapid]';
-const varmeplanMinimapIdDev = '12c3cde2-dda6-406b-8074-4a5d618b268b';
-export const varmeplanMinimapId = varmeplanMinimapIdParam.includes('[') ? varmeplanMinimapIdDev : varmeplanMinimapIdParam;
+const supplyareaMinimapIdParam = '[module.varmeplan.supplyarea.minimapid]';
+const supplyareaMinimapIdDev = '12c3cde2-dda6-406b-8074-4a5d618b268b';
+export const supplyareaMinimapId = supplyareaMinimapIdParam.includes('[') ? supplyareaMinimapIdDev : supplyareaMinimapIdParam;
+
+const villagesMinimapIdParam = '[module.varmeplan.villages.minimapid]';
+const villagesMinimapIdDev = 'e53080e9-fbde-4e12-8ba3-a46e4546c94e';
+export const villagesMinimapId = villagesMinimapIdParam.includes('[') ? villagesMinimapIdDev : villagesMinimapIdParam;
+
+const cottageareasMinimapIdParam = '[module.varmeplan.cottageareas.minimapid]';
+const cottageareasMinimapIdDev = '1e33f516-c9fd-41a6-a761-eb1d2890c0af';
+export const cottageareasMinimapId = cottageareasMinimapIdParam.includes('[') ? cottageareasMinimapIdDev : cottageareasMinimapIdParam;
+
+const opencountryMinimapIdParam = '[module.varmeplan.opencountry.minimapid]';
+const opencountryMinimapIdDev = '5490d72f-1077-4969-8c3c-1e6cc7fd012f';
+export const opencountryMinimapId = opencountryMinimapIdParam.includes('[') ? opencountryMinimapIdDev : opencountryMinimapIdParam;
