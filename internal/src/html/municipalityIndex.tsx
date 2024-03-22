e1bffd3c9a4e6160e7096342a2a7d8120af1441d
MarvinPoulsen <marpo@lolland.dk>
2024-03-22 11:22:40 +0100

    changed index.scss to sharedStyles.scss

--- internal/src/html/municipalityIndex.tsx
+++ internal/src/html/municipalityIndex.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '../pages/Municipality';
-import '../index.scss';
+import '../sharedStyles.scss';
 
 const MOUNT_NODE = document.getElementById('municipality');
 const root = createRoot(MOUNT_NODE as Element);
--- internal/src/html/villagesIndex.tsx
+++ internal/src/html/villagesIndex.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '../pages/Villages';
-import '../index.scss';
+import '../sharedStyles.scss';
 
 const MOUNT_NODE = document.getElementById('villages');
 const root = createRoot(MOUNT_NODE as Element);
